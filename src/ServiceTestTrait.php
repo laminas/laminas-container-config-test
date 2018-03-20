df9dfb1a53a72220e595740119e0cd263064fcb7
Matthew Weier O'Phinney <matthew@zend.com>
2018-03-20 15:44:12 -0500

    Updates name for ServiceTestTrait
    - to testFetchingServiceReturnsSameInstance

--- src/ServiceTestTrait.php
+++ src/ServiceTestTrait.php
@@ -11,7 +11,7 @@ namespace Zend\ContainerConfigTest;
 
 trait ServiceTestTrait
 {
-    public function testService() : void
+    public function testFetchingServiceReturnsSameInstance() : void
     {
         $service = new TestAsset\Service();
         $config = [
